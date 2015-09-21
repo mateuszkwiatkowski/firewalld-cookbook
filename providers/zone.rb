5c9c352e6beca388516f738e02dba11083ebffb0
Mateusz Kwiatkowski <kwiat@panic.pl>
2015-09-21 17:03:06 +0200

    Fix wrong argument name

--- providers/zone.rb
+++ providers/zone.rb
@@ -5,11 +5,11 @@
 # Copyright:: 2015, Jeff Hutchison
 
 action :new do
-  e = execute "create new zone #{new_resource.service}" do
-    not_if "firewall-cmd --permanent --get-zones | grep #{new_resource.service}"
+  e = execute "create new zone #{new_resource.zone}" do
+    not_if "firewall-cmd --permanent --get-zones | grep #{new_resource.zone}"
     command(<<-EOC)
-      firewall-cmd --new-zone=#{new_resource.service}
-      firewall-cmd --permanent --new-zone=#{new_resource.service}
+      firewall-cmd --new-zone=#{new_resource.zone}
+      firewall-cmd --permanent --new-zone=#{new_resource.zone}
     EOC
   end
   new_resource.updated_by_last_action(e.updated_by_last_action?)
